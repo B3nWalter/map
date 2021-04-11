6ba4248da6c0cb690dff0738a8d4e829e5cf10b7
B3nWalter <60394161+B3nWalter@users.noreply.github.com>
2021-04-11 22:30:24 +0000

    Deploying to gh-pages from master @ 1f94d15623c09d54c508382f19a14287d090c4c8 🚀

---
+++ gather_chairs_1.3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="gather_chairs_1.3" tilewidth="32" tileheight="32" tilecount="800" columns="25">
+ <image source="gather_chairs_1.3.png" width="800" height="1024"/>
+</tileset>
