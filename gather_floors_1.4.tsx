1f94d15623c09d54c508382f19a14287d090c4c8
Ben <bwalter@owncloud.com>
2021-04-12 00:30:00 +0200

    test1

---
+++ gather_floors_1.4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="gather_floors_1.4" tilewidth="32" tileheight="32" tilecount="2560" columns="40">
+ <image source="gather_floors_1.4.png" width="1280" height="2048"/>
+</tileset>
