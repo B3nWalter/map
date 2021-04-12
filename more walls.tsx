40feb6ac1fb2f0f189948e64fd2197d56d153704
Ben <bwalter@owncloud.com>
2021-04-12 03:26:47 +0200

    first draft

---
+++ more walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="more walls" tilewidth="32" tileheight="32" tilecount="1904" columns="28">
+ <image source="more walls.png" width="900" height="2176"/>
+</tileset>
---
+++ temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="temp" tilewidth="32" tileheight="32" tilecount="7125" columns="25">
+ <image source="temp.png" width="800" height="9120"/>
+</tileset>
